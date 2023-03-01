519090fc4db8f32fabafdf587f2c1f936ed0176e
Fanny Zheng <91427854+fanzzz368@users.noreply.github.com>
2023-03-01 18:33:37 -0500

    Revert "Revert "Implemented the basics for the form components""
    This reverts commit 7bd312e2c4b445a008c7a94477a64e7c3effbb01.

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-import NxWelcome from './nx-welcome';
 
 const StyledApp = styled.div`
   // Your style here
@@ -8,7 +7,6 @@ const StyledApp = styled.div`
 export function App() {
   return (
     <StyledApp>
-      <NxWelcome title="frontend" />
     </StyledApp>
   );
 }
