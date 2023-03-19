37bae458d47d077ff3dcd948deb7d7ae9d734780
jennya alexeyeva <jennyaalexeyeva@gmail.com>
2023-03-19 17:44:19 -0400

    submitted forms page wip

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,5 +1,8 @@
 import styled from '@emotion/styled';
 import NxWelcome from './nx-welcome';
+import {Routes, Route, BrowserRouter} from 'react-router-dom';
+import ViewFormsList from '../ViewFormsList';
+import {ChakraProvider} from "@chakra-ui/react";
 
 const StyledApp = styled.div`
   // Your style here
@@ -7,9 +10,16 @@ const StyledApp = styled.div`
 
 export function App() {
   return (
-    <StyledApp>
-      <NxWelcome title="frontend" />
-    </StyledApp>
+    <ChakraProvider>
+      <BrowserRouter>
+        <StyledApp>
+          <Routes>
+            <Route path="/" element={<NxWelcome title="frontend" />} />
+            <Route path="/viewForms" element={<ViewFormsList />} />
+          </Routes>
+        </StyledApp>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
