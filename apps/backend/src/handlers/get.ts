f938e73f89d294a4ea49b6948ec530a672785813
nate-kirschner <77256794+nate-kirschner@users.noreply.github.com>
2023-03-27 19:53:52 -0400

    changed spaces to 2

--- apps/backend/src/handlers/get.ts
+++ apps/backend/src/handlers/get.ts
@@ -5,21 +5,21 @@ import { APIGatewayEvent } from 'aws-lambda';
  * A simple example includes a HTTP get method that returns "Hello world!".
  */
 export const getHandler = async (event: APIGatewayEvent) => {
-    if (event.httpMethod !== 'GET') {
-        throw new Error(
-            `getHandler only accept GET method, you tried: ${event.httpMethod}`
-        );
-    }
-    // All log statements are written to CloudWatch
-    console.info('received:', event);
-
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello world!'),
-    };
-    // All log statements are written to CloudWatch
-    console.log(
-        `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+  if (event.httpMethod !== 'GET') {
+    throw new Error(
+      `getHandler only accept GET method, you tried: ${event.httpMethod}`
     );
-    return response;
+  }
+  // All log statements are written to CloudWatch
+  console.info('received:', event);
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello world!'),
+  };
+  // All log statements are written to CloudWatch
+  console.log(
+    `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+  );
+  return response;
 };
