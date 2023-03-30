d530a4ecb3a4f07922a70085d5a2e2a0a60bfe16
Megan Li <meganlidou@gmail.com>
2023-03-30 00:25:43 -0400

    update tests

--- apps/frontend/src/app/app.spec.tsx
+++ apps/frontend/src/app/app.spec.tsx
@@ -2,14 +2,9 @@ import { render } from '@testing-library/react';
 
 import App from './app';
 
-// describe('App', () => {
-//   it('should render successfully', () => {
-//     const { baseElement } = render(<App />);
-//     expect(baseElement).toBeTruthy();
-//   });
-
-//   it('should have a greeting as the title', () => {
-//     const { getByText } = render(<App />);
-//     expect(getByText(/Welcome frontend/gi)).toBeTruthy();
-//   });
-// });
+describe('App', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<App isPassedToWithAuthenticator={true} />);
+    expect(baseElement).toBeTruthy();
+  });
+});
