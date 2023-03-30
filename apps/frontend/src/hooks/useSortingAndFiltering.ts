e33230ced22155c948cc685afe1f4f08d98c4fcb
coler19 <reardon.co@husky.neu.edu>
2023-03-30 16:24:15 -0400

    remove types from hook

--- apps/frontend/src/hooks/useSortingAndFiltering.ts
+++ apps/frontend/src/hooks/useSortingAndFiltering.ts
@@ -1,64 +1,42 @@
-import { useEffect, useState } from 'react';
-import {
-  CancersDropdownValues,
-  HospitalsDropdownValues,
-} from '../components/form/MedicalForm';
+import { useEffect, useMemo, useState } from 'react';
 import type FormData from '../types/FormData';
-
-export enum SortField {
-  NAME = 'childsName',
-  DATE = 'date',
-}
-export enum SortOrder {
-  'ASC',
-  'DESC',
-}
-
-type Options = {
-  sorting?: {
-    field: keyof FormData;
-    sortOrder: SortOrder;
-  };
-  filtering?: {
-    filters: {
-      field: keyof FormData;
-      value: FormData[keyof FormData];
-    }[];
-    hospital?: HospitalsDropdownValues;
-    cancerType?: CancersDropdownValues;
-  };
-};
+import { Options, SortOrder } from '../types/SortAndFilter';
 
 const useSortingAndFiltering = (list: FormData[], options: Options) => {
   const [listState, setListState] = useState(list);
 
+  const reducedData = useMemo(() => reduceData(list, options), [list, options]);
+
   useEffect(() => {
-    const newData = reduceData(list, options);
-    setListState(newData);
-  }, [list, options]);
+    setListState(reducedData);
+  }, [reducedData]);
 
   return listState;
 };
 
 const reduceData = (list: FormData[], options: Options) => {
   const { sorting, filtering } = options;
+  let newData = [...list];
+
+  if (sorting) {
+    const { field, sortOrder } = sorting;
+    if (sortOrder === SortOrder.ASC) {
+      newData.sort((a, b) => (b[field] > a[field] ? -1 : 1));
+    } else {
+      newData.sort((a, b) => (a[field] > b[field] ? -1 : 1));
+    }
+  }
+
   if (filtering) {
     const { filters } = filtering;
     for (const filter of filters) {
       const { field, value } = filter;
 
-      list = list.filter((formEntry) => formEntry[field] === value);
+      newData = list.filter((formEntry) => formEntry[field] === value);
     }
   }
-  if (sorting) {
-    const { field, sortOrder } = sorting;
-    if (sortOrder === SortOrder.ASC) {
-      list.sort((a, b) => (b[field] > a[field] ? -1 : 1));
-    } else {
-      list.sort((a, b) => (a[field] > b[field] ? -1 : 1));
-    }
-  }
-  return list;
+
+  return newData;
 };
 
 export default useSortingAndFiltering;
