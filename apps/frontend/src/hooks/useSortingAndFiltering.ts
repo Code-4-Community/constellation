0c1f2ebfd4ddf2f4d85d02724c5aac2ba4f66f91
coler19 <reardon.co@husky.neu.edu>
2023-03-29 21:08:59 -0400

    Add basic hook layout

---
+++ apps/frontend/src/hooks/useSortingAndFiltering.ts
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import {
+  CancersDropdownValues,
+  HospitalsDropdownValues,
+} from '../components/form/MedicalForm';
+import type FormData from '../types/FormData';
+
+enum SortOrder {
+  'ASC',
+  'DESC',
+}
+
+type Options = {
+  sorting?: {
+    name?: SortOrder;
+    date?: SortOrder;
+  };
+  filtering?: {
+    hospital?: HospitalsDropdownValues;
+    cancerType?: CancersDropdownValues;
+  };
+};
+
+const useSortingAndFiltering = (list: FormData[], options: Options) => {
+  const [optionsState, setOptionsState] = useState(options);
+  const [listState, setListState] = useState(list);
+
+  useEffect(() => {
+    setOptionsState(options);
+    setListState(list);
+    const newData = reduceData(listState, optionsState);
+    setListState(newData);
+  }, [list, options]);
+
+  return listState;
+};
+
+const reduceData = (list: FormData[], options: Options) => {
+  const { sorting, filtering } = options;
+  if (sorting) {
+    for (const [key, value] of Object.entries(sorting)) {
+      // TODO: Do sorting algorithm here
+    }
+  }
+  if (filtering) {
+    for (const [key, value] of Object.entries(filtering)) {
+      // TODO: Do filtering algorithm here
+      list.filter((formEntry) => {
+        /**
+         * This is gross! Here are the stackoverflows I used for reference:
+         * https://stackoverflow.com/questions/455338/how-do-i-check-if-an-object-has-a-key-in-javascript
+         * https://stackoverflow.com/questions/57086672/element-implicitly-has-an-any-type-because-expression-of-type-string-cant-b
+         */
+        if (Object.prototype.hasOwnProperty.call(formEntry, key)) {
+          return formEntry[key as keyof FormData] === value;
+        } else return false;
+      });
+    }
+  }
+};
