8588e2f2d2b228455d7380fa74aad8e041da32e2
coler19 <reardon.co@husky.neu.edu>
2023-04-01 14:36:30 -0400

    Fix filter function to use OR with multiple filters

--- apps/frontend/src/hooks/useSortingAndFiltering.ts
+++ apps/frontend/src/hooks/useSortingAndFiltering.ts
@@ -3,6 +3,12 @@ import SortOrder from '../enums/SortOrder';
 import type FormData from '../types/FormData';
 import { Options } from '../types/SortAndFilter';
 
+/**
+ * This hook is for sorting, filtering, and searching an array of form data. Currently, you can filter by any number of
+ * @param list
+ * @param options
+ * @returns
+ */
 const useSortingAndFiltering = (list: FormData[], options: Options) => {
   const [listState, setListState] = useState(list);
 
@@ -15,21 +21,108 @@ const useSortingAndFiltering = (list: FormData[], options: Options) => {
   return listState;
 };
 
-const reduceData = (list: FormData[], options: Options) => {
+/**
+ * A function that represents the driver for sorting, searching, and filtering a list of form entries.
+ * @param list A list of form entries
+ * @param options An object that represents every sorting, filtering, and searching option that the user wants
+ * @returns A list of form entries that pass all filtering, sorting, and searching conditions
+ */
+const reduceData = (list: FormData[], options: Options): FormData[] => {
   const { sorting, filtering, searching } = options;
-  let newData = [...list];
 
   if (filtering) {
-    const { filters } = filtering;
-    for (const filter of filters) {
-      const { field, value } = filter;
-
-      newData = list.filter((formEntry) => formEntry[field] === value);
-    }
+    const filteredList = filterData(list, { filtering });
+    if (filteredList) return filteredList;
   }
 
+  // TODO: Make searching work with numbers and recursively through all child objects
   if (searching) {
-    const { searchTerm } = searching;
+    const searchedList = searchData(list, { searching });
+  }
+
+  if (sorting) {
+    const sortedList = sortData(list, { sorting });
+  }
+
+  return list;
+};
+
+/**
+ * A function that performs filtering on a list of form objects.
+ * @param list A list of form objects
+ * @param filteringObject Options pertaining to filters
+ * @returns A list of filtered form objects
+ */
+const filterData = (
+  list: FormData[],
+  filteringObject: Pick<Options, 'filtering'>
+) => {
+  if (filteringObject && filteringObject.filtering) {
+    const { filters } = filteringObject.filtering;
+    const filteredList = list.filter((formEntry) => {
+      for (const filter of filters) {
+        const { field: filterField, value: filterValue } = filter;
+        if (formEntry[filterField] === filterValue) return true;
+      }
+      return false;
+    });
+
+    return filteredList;
+  }
+
+  //   if (filteringObject.filtering) {
+  //     const { filters } = filteringObject.filtering;
+  //     let newData: FormData[] = [];
+
+  //     for (const filter of filters) {
+  //       const { field, value } = filter;
+
+  //       const filteredData = list.filter(
+  //         (formEntry) => formEntry[field] === value
+  //       );
+  //       newData = [...filteredData];
+  //     }
+  //     return newData;
+  //   }
+};
+
+/**
+ * A function that performs sorting on a list of form objects. Sorting can happen on any object parameter
+ * in ascending or descending order, and only one paramter can be sorted on at a time.
+ * @param list a list of form objects
+ * @param sortingObject Options pertaining to sorting
+ * @returns A list of sorted form objects
+ */
+const sortData = (
+  list: FormData[],
+  sortingObject: Pick<Options, 'sorting'>
+) => {
+  if (sortingObject.sorting) {
+    const newData = [...list];
+    const { field, sortOrder } = sortingObject.sorting;
+    if (sortOrder === SortOrder.ASC) {
+      newData.sort((a, b) => (b[field] > a[field] ? -1 : 1));
+    } else {
+      newData.sort((a, b) => (a[field] > b[field] ? -1 : 1));
+    }
+
+    return newData;
+  }
+};
+
+/**
+ * A function that performs a search through every value in each form object.
+ * @param list a list of form objects
+ * @param searchingObject Options pertaining to searching
+ * @returns A list of searched form objects
+ */
+const searchData = (
+  list: FormData[],
+  searchingObject: Pick<Options, 'searching'>
+) => {
+  if (searchingObject.searching) {
+    const { searchTerm } = searchingObject.searching;
+    let newData: FormData[] = [];
     newData = newData.filter((form) => {
       const formValues = Object.values(form);
       const lowercaseFormValues = formValues.map((value) => {
@@ -44,18 +137,9 @@ const reduceData = (list: FormData[], options: Options) => {
       }
       return false;
     });
-  }
 
-  if (sorting) {
-    const { field, sortOrder } = sorting;
-    if (sortOrder === SortOrder.ASC) {
-      newData.sort((a, b) => (b[field] > a[field] ? -1 : 1));
-    } else {
-      newData.sort((a, b) => (a[field] > b[field] ? -1 : 1));
-    }
+    return newData;
   }
-
-  return newData;
 };
 
 export default useSortingAndFiltering;
