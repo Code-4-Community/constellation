8d7d5f79ea7249130a2f3f7fc54dba75c9b2a867
coler19 <reardon.co@husky.neu.edu>
2023-04-16 19:34:49 -0400

    update ViewFormsList styling

--- apps/frontend/src/components/ViewFormsList/ViewFormsList.tsx
+++ apps/frontend/src/components/ViewFormsList/ViewFormsList.tsx
@@ -14,18 +14,28 @@ import {
   Thead,
   Tr,
   Link,
+  GridItem,
 } from '@chakra-ui/react';
 import { getAllForms } from '../../utils/sendRequest';
-import { FormData } from '../../types/formData';
+import { FormData, FormDataNestedKeys } from '../../types/formData';
 import useSortingAndFiltering from '../../hooks/useSortingAndFiltering';
-import { Options } from '../../types/SortAndFilter';
+import { Filter, Filtering, Options } from '../../types/SortAndFilter';
 import FormListRow from './FormListRow';
+import FormListFilter from './FormListFilter';
+import { Grid } from '@aws-amplify/ui-react';
+
+export enum FormDataToEnglish {
+  'date' = 'Last Updated',
+  'guardianForm.childsName' = 'Child Name',
+  'guardianForm.dob' = 'Date of Birth',
+  'medicalForm.hospital' = 'Hospital',
+  'guardianForm.address' = 'Location',
+}
 
 export default function ViewFormsList() {
   const [forms, setForms] = useState<FormData[] | null>(null);
-  const [options, setOptions] = useState<Options>({});
 
-  const filteredForms = useSortingAndFiltering(forms, options);
+  const { listState: filteredForms } = useSortingAndFiltering();
 
   const getForms = async () => {
     const allForms = await getAllForms();
@@ -45,16 +55,7 @@ export default function ViewFormsList() {
         <option value="name">Sort by Name</option>
         <option value="recency">Sort by Last Updated</option>
       </Select>
-      <Table marginLeft="auto" marginRight="auto" width="98%" variant="striped">
-        <Thead>
-          <Tr>
-            <Th>Last Updated</Th>
-            <Th>Child Name</Th>
-            <Th>Date of Birth</Th>
-            <Th>Hospital</Th>
-            <Th>Location</Th>
-          </Tr>
-        </Thead>
+      <Grid templateColumns="1fr 3fr">
         {filteredForms === null || filteredForms.length === 0 ? (
           <Flex>
             <Spacer />
@@ -62,20 +63,63 @@ export default function ViewFormsList() {
             <Spacer />
           </Flex>
         ) : (
-          <Tbody>
-            {filteredForms.map((form) => (
-              <FormListRow
-                id={form.id}
-                date={form.guardianForm.date}
-                childsName={form.guardianForm.childsName}
-                dob={form.guardianForm.dob}
-                hospital={form.medicalForm.hospital}
-                address={form.guardianForm.address}
-              />
-            ))}
-          </Tbody>
+          <GridItem colSpan={1}>
+            {Object.entries(FormDataToEnglish).map(
+              ([formKey, englishValue], index) => {
+                return (
+                  <FormListFilter
+                    key={index}
+                    dataField={formKey as FormDataNestedKeys}
+                    dataLabel={englishValue}
+                    filterValues={getUniqueValuesFromKey(
+                      filteredForms,
+                      formKey as FormDataNestedKeys
+                    )}
+                  />
+                );
+              }
+            )}
+          </GridItem>
         )}
-      </Table>
+        <GridItem colSpan={1}>
+          <Table
+            marginLeft="auto"
+            marginRight="auto"
+            width="98%"
+            variant="striped"
+          >
+            <Thead>
+              <Tr>
+                <Th>Last Updated</Th>
+                <Th>Child Name</Th>
+                <Th>Date of Birth</Th>
+                <Th>Hospital</Th>
+                <Th>Location</Th>
+              </Tr>
+            </Thead>
+            {filteredForms === null || filteredForms.length === 0 ? (
+              <Flex>
+                <Spacer />
+                <Spinner size="xl" />
+                <Spacer />
+              </Flex>
+            ) : (
+              <Tbody>
+                {filteredForms.map((form) => (
+                  <FormListRow
+                    id={form.id}
+                    date={form.guardianForm.date}
+                    childsName={form.guardianForm.childsName}
+                    dob={form.guardianForm.dob}
+                    hospital={form.medicalForm.hospital}
+                    address={form.guardianForm.address}
+                  />
+                ))}
+              </Tbody>
+            )}
+          </Table>
+        </GridItem>
+      </Grid>
     </Box>
   );
 }
