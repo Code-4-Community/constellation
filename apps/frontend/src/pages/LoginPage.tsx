ddb501ab22a044a9191fd805d1a2207f497d870c
Fanny Zheng <91427854+fanzzz368@users.noreply.github.com>
2023-04-17 17:49:57 -0400

    added header to login page + centered the login popup

---
+++ apps/frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import { Container } from '@chakra-ui/react';
+import React from 'react';
+import Header from '../components/header/Header';
+import { Login } from '../components/auth/Login';
+
+const LoginPage: React.FC = () => (
+    <Container>
+      <Header />
+      <Login />
+    </Container>
+  );
+  
+  export default LoginPage;
