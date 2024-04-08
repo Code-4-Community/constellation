00808c4157113d1fa4d531aa68ac95888617e122
nate-kirschner <77256794+nate-kirschner@users.noreply.github.com>
2024-04-08 16:58:16 -0400

    updated logo to new svg

--- apps/frontend/src/components/header/Header.tsx
+++ apps/frontend/src/components/header/Header.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react';
-import { Box, Image, Link } from '@chakra-ui/react';
-import Logo from '../header/Swgologo.jpeg';
+import { Box, Link } from '@chakra-ui/react';
+import Logo from './Logo';
 
 const Header: React.FC = () => {
-
   return (
     <Box display="flex" pr="8" pl="8" py="4" justifyContent="space-between">
       <Link href="https://www.tswgo.org/">
-        <Image width="128px" height="45.8px" src={Logo} />
+        <Logo />
       </Link>
     </Box>
   );
