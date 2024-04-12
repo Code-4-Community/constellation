27ebd27ea09be6de3d12580a89fc80b81e2349fe
Sam <shortdriva@aol.com>
2024-04-12 18:41:34 -0400

    Update documentation

--- apps/frontend/src/components/form/FormSection.tsx
+++ apps/frontend/src/components/form/FormSection.tsx
@@ -9,29 +9,23 @@ interface FormSectionProps {
 
 /**
  * Displays the child form HTML passed to it, surrounded by the form section styling
- * (including the section title, back and next buttons, and a progress bar).
+ * (including the section title and background boxes).
  */
 export default function FormSection({ title, children }: FormSectionProps) {
   return (
-    <Box>
-      <Box bg="#422669" borderRadius="10px" padding="3" marginBottom="3">
-        <Box color="#FFFFFF" marginBottom="3">
-          {title}
-        </Box>
-        <Flex
-          bg="#FFFFFF"
-          flexDirection="column"
-          rowGap="20px"
-          borderRadius="10px"
-          padding="20px"
-        >
-          {children}
-        </Flex>
+    <Box bg="#422669" borderRadius="10px" padding="3" marginBottom="3">
+      <Box color="#FFFFFF" marginBottom="3">
+        {title}
       </Box>
-      {/* <Flex justifyContent="space-between">
-        <NextButton option="Previous" nextStep={() => {}} />
-        <NextButton option="Next" nextStep={() => {}} />
-      </Flex> */}
+      <Flex
+        bg="#FFFFFF"
+        flexDirection="column"
+        rowGap="20px"
+        borderRadius="10px"
+        padding="20px"
+      >
+        {children}
+      </Flex>
     </Box>
   );
 }
