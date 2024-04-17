50101badffae31b092cfd0711f58b52b6e73e701
nate-kirschner <77256794+nate-kirschner@users.noreply.github.com>
2024-04-17 19:33:42 -0400

    added validation for forms from backend

--- apps/backend/src/handlers/getFormById.ts
+++ apps/backend/src/handlers/getFormById.ts
@@ -1,6 +1,7 @@
 import { APIGatewayEvent } from 'aws-lambda';
 import { fetchDocumentById } from '../db/utils.js';
 import { executeHandler } from '../utils/executeHandler.js';
+import { sanitizeFormList } from '../utils/sanitizeForms.js';
 import { validateMethodType } from '../utils/validateMethodType.js';
 
 /**
@@ -19,7 +20,12 @@ export const getFormByIdHandler = async (event: APIGatewayEvent) => {
   const handlerFunction = async () => {
     const id = event.pathParameters!.id!;
     const form = await fetchDocumentById(id);
-    return JSON.stringify(form);
+    const validatedForm = sanitizeFormList(form);
+    if (validatedForm.length !== 0) {
+      return JSON.stringify(validatedForm);
+    } else {
+      throw new Error(`This form does not match the expected type.`);
+    }
   };
 
   return executeHandler(event, handlerFunction);
--- apps/frontend/src/components/ViewFormsList/ViewFormsList.tsx
+++ apps/frontend/src/components/ViewFormsList/ViewFormsList.tsx
@@ -26,6 +26,54 @@ import {
 } from '../../utils/sortFunctions';
 import CSVImportButton from './CSVImportButton';
 import ViewFormsOptions from './ViewFormsOptions';
+import { formSchema } from '../../types/formSchema';
+
+const testData = {
+  adminNotes: [],
+  medicalForm: {
+    notes: '',
+    socialWorkersEmail: 'email@email.com',
+    date: '1010-10-10T04:56:02.000Z',
+    signature: 'Signature',
+    medicalProfessionalTitle: 'Title',
+    medicalProfessionalName: 'Name',
+    descriptionOfCondition: '',
+    phone: '215-222-2222',
+    address: {
+      zipcode: '12345',
+      state: 'MA',
+      city: 'City',
+      street: 'Address',
+    },
+    otherHospital: '',
+    hospital: 'DANAHOSPITAL',
+    childsPhysician: 'Name',
+    dateOfDiagnosis: '1010-10-10T04:56:02.000Z',
+    otherDiagnosis: '',
+    childsDiagnosis: 'LYMPHOMAS',
+  },
+  guardianForm: {
+    date: '1010-10-10T04:56:02.000Z',
+    signature: 'Signature',
+    intendedUseOfGrant: 'Use of grant',
+    requestedGrantAmount: 3000,
+    email: 'email@email.com',
+    cellPhone: '215-222-2222',
+    phone: '215-222-2222',
+    address: {
+      zipcode: '12345',
+      state: 'ME',
+      city: 'City',
+      street: 'Address',
+    },
+    guardianName: 'Name',
+    gender: 'Male',
+    dob: '1010-10-10T04:56:02.000Z',
+    childsName: 'Name',
+    notes: 'Notes',
+  },
+  id: 'ab0c6c23-0177-454b-aa2a-b56c805ca2bc',
+};
 
 export default function ViewFormsList() {
   const [forms, setForms] = useState<FormData[]>([]);
@@ -42,8 +90,22 @@ export default function ViewFormsList() {
 
   const getForms = async () => {
     const allForms = await getAllForms();
-    setForms(allForms);
-    setAllForms(allForms);
+    const validatedForms = await validateForms(allForms);
+    setForms(validatedForms);
+    setAllForms(validatedForms);
+  };
+
+  const validateForms = async (forms: any[]) => {
+    const validatedForms = [] as FormData[];
+    for (const f of forms) {
+      try {
+        await formSchema.validate(f);
+        validatedForms.push(f as any as FormData);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    return validatedForms;
   };
 
   useEffect(() => {
@@ -66,7 +128,7 @@ export default function ViewFormsList() {
         <Center mb={1}>
           <Heading
             sx={{
-              fontSize: "52px",
+              fontSize: '52px',
             }}
           >
             Submitted Forms
@@ -87,11 +149,11 @@ export default function ViewFormsList() {
           <Tr>
             <Th></Th>
             <Th></Th>
-            <Th sx={{ color: "#000000", fontSize: "23px" }}>Last Updated</Th>
-            <Th sx={{ color: "#000000", fontSize: "23px" }}>Child Name</Th>
-            <Th sx={{ color: "#000000", fontSize: "23px" }}>Date of Birth</Th>
-            <Th sx={{ color: "#000000", fontSize: "23px" }}>Hospital</Th>
-            <Th sx={{ color: "#000000", fontSize: "23px" }}>Location</Th>
+            <Th sx={{ color: '#000000', fontSize: '23px' }}>Last Updated</Th>
+            <Th sx={{ color: '#000000', fontSize: '23px' }}>Child Name</Th>
+            <Th sx={{ color: '#000000', fontSize: '23px' }}>Date of Birth</Th>
+            <Th sx={{ color: '#000000', fontSize: '23px' }}>Hospital</Th>
+            <Th sx={{ color: '#000000', fontSize: '23px' }}>Location</Th>
           </Tr>
         </Thead>
         {forms === null ? (
@@ -107,40 +169,40 @@ export default function ViewFormsList() {
                 key={form.id}
                 onClick={() => navigateToForm(form.id)}
                 sx={{
-                  color: index % 2 === 0 ? "#000000" : "#555555",
+                  color: index % 2 === 0 ? '#000000' : '#555555',
                 }}
               >
-                <Td sx={{ fontSize: "20px" }}>{`${index + 1}.`}</Td>
+                <Td sx={{ fontSize: '20px' }}>{`${index + 1}.`}</Td>
                 <Td
                   style={{
-                    color: "#EA6824",
-                    fontSize: "24pt",
+                    color: '#EA6824',
+                    fontSize: '24pt',
                   }}
                 >
-                  {form.read === undefined || !form.read ? "●" : ""}
+                  {form.read === undefined || !form.read ? '●' : ''}
                 </Td>
-                <Td sx={{ fontSize: "20px" }}>
+                <Td sx={{ fontSize: '20px' }}>
                   {form.adminNotes.length > 0
                     ? new Date(
-                        form.adminNotes[0].updatedAt,
+                        form.adminNotes[0].updatedAt
                       ).toLocaleDateString()
                     : new Date(
-                        form.financialAssistanceForm.date,
+                        form.financialAssistanceForm.date
                       ).toLocaleDateString()}
                 </Td>
-                <Td sx={{ fontSize: "20px" }}>
+                <Td sx={{ fontSize: '20px' }}>
                   {form.financialAssistanceForm.childsName}
                 </Td>
-                <Td sx={{ fontSize: "20px" }}>
+                <Td sx={{ fontSize: '20px' }}>
                   {new Date(
-                    form.financialAssistanceForm.dob,
+                    form.financialAssistanceForm.dob
                   ).toLocaleDateString()}
                 </Td>
-                <Td sx={{ fontSize: "20px" }}>
+                <Td sx={{ fontSize: '20px' }}>
                   {form.financialAssistanceForm.hospital}
                 </Td>
                 <Td
-                  sx={{ fontSize: "20px" }}
+                  sx={{ fontSize: '20px' }}
                 >{`${form.financialAssistanceForm.hospitalAddress.city}, ${form.financialAssistanceForm.hospitalAddress.state}`}</Td>
               </Tr>
             ))}
